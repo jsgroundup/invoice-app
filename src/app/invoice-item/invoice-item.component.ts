29fde45009b3b34af2f88737780e07ee72f5fb91
KBismark <bismark.yamoah@amalitech.com>
2024-12-28 07:13:41 +0000

    Invoice item component

---
+++ src/app/invoice-item/invoice-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '[invoice-listitem]',
+  standalone: true,
+  imports: [],
+  templateUrl: './invoice-item.component.html',
+  styleUrl: './invoice-item.component.css'
+})
+export class InvoiceItemComponent {
+
+}
