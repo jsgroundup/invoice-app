fe61d9ce1ef4cd66461b5ed56a5430d77f9316ad
KBismark <bismark.yamoah@amalitech.com>
2024-12-28 07:17:32 +0000

    Update app's component to include invoice items and app' header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { InvoiceItemComponent } from './invoice-item/invoice-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, InvoiceItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
