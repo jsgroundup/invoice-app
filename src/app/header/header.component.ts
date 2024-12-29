814f8192334f68713763a30bb8834799bf57d365
KBismark <bismark.yamoah@amalitech.com>
2024-12-29 02:57:14 +0000

    Use data-theme as a selector for dark and light themes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { IconComponent } from '../icon/icon.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: '[app-header]',
   standalone: true,
-  imports: [],
+  imports: [IconComponent, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  theme: ThemeMode = 'light'
+
+  get isLightMode(){
+    return this.theme === 'light';
+  }
+
+  toggleTheme(){
+    const theme: ThemeMode = this.theme = this.isLightMode ? 'dark' : 'light';
+    localStorage.setItem('theme',theme);
+    document.documentElement.setAttribute('data-theme', theme);
+  }
+
+  constructor(){
+    let theme = localStorage.getItem('theme') as ThemeMode|null;
+    if(theme&&['dark'].includes(theme)){
+      this.theme = theme;
+      document.documentElement.setAttribute('data-theme', theme);
+
+    }
+  }
+}
+
+type ThemeMode = 'dark'|'light'
