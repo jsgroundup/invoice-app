938d54330422efaeee8e4b6b482c4c14cf52040c
KBismark <bismark.yamoah@amalitech.com>
2024-12-29 04:06:47 +0000

    Make invoice list item component responsive

--- src/app/invoice-item/invoice-item.component.ts
+++ src/app/invoice-item/invoice-item.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IconComponent } from '../icon/icon.component';
+import { Badges, StatusBadgeComponent } from '../status-badge/status-badge.component';
 
 @Component({
   selector: '[invoice-listitem]',
   standalone: true,
-  imports: [IconComponent],
+  imports: [IconComponent, StatusBadgeComponent],
   templateUrl: './invoice-item.component.html',
   styleUrl: './invoice-item.component.css'
 })
 export class InvoiceItemComponent {
-
+  @Input({required: true}) status!: Badges
 }
