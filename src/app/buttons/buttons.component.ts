0242336ae88dc539594baab4d3dcfdc37b16ae66
KBismark <bismark.yamoah@amalitech.com>
2024-12-29 04:05:54 +0000

    Create button components

---
+++ src/app/buttons/buttons.component.ts
@@ -0,0 +1,21 @@
+import { Component, HostBinding, Input } from '@angular/core';
+
+@Component({
+  selector: '[app-button]',
+  standalone: true,
+  imports: [],
+  templateUrl: './buttons.component.html',
+  styleUrl: './buttons.component.css',
+})
+export class ButtonsComponent {
+  @Input() buttonType?: ButtonTypes;
+  @Input({ required: true }) title = '';
+
+  @HostBinding('class') get className(): (ButtonTypes) {
+    return ['neutral', 'danger', 'default'].includes(this.buttonType!)
+      ? this.buttonType!
+      : 'default';
+  }
+}
+
+type ButtonTypes = 'danger' | 'default' | 'neutral';
