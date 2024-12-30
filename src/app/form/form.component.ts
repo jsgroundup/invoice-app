e29a5ac373fde5ea90eb6c7b6728c344308966dc
KBismark <bismark.yamoah@amalitech.com>
2024-12-30 03:30:55 +0000

    Update form component to include popup overlay

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IconComponent } from '../icon/icon.component';
 import { ButtonsComponent } from '../buttons/buttons.component';
+import { PopupOverlayComponent } from '../popup-overlay/popup-overlay.component';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [IconComponent, ButtonsComponent],
+  imports: [IconComponent, ButtonsComponent, PopupOverlayComponent],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
