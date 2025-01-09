08c6d4494161fc5ad31a183231253c7fa9a4cc35
KBismark <bismark.yamoah@amalitech.com>
2025-01-09 16:12:00 +0000

    Update reducers and actions to handle adding and updationg of new invoice data

--- src/store/actions/invoices.ts
+++ src/store/actions/invoices.ts
@@ -5,8 +5,19 @@ import { Badges } from "../../app/status-badge/status-badge.component";
 const getAction = (action: string)=> `[Invoice] ${action}`
 
 export default {
+  // Delete invoice
   delete: createAction(getAction('delete'), props<{ id: string }>()),
+
+  // Add invoice
   add: createAction(getAction('add'), props<Invoice>()),
-  updateStatus: createAction(getAction('update status'), props<{ id: string, status: Badges }>()),
+
+  // Update invoice
+  update: createAction(getAction('update'), props<Invoice>()),
+
+  // Update invoice status
+  updateStatus: createAction(
+    getAction('update status'),
+    props<{ id: string; status: Badges }>()
+  ),
 };
 
--- src/store/reducers/invoices.ts
+++ src/store/reducers/invoices.ts
@@ -7,13 +7,27 @@ export const invoiceReducer = createReducer(
   on(Actions.delete, (store, { id }) => {
     return store.filter((data) => data.id !== id);
   }),
+
   on(Actions.add, (store, data) => {
     return [data, ...store];
   }),
+
   on(Actions.updateStatus, (store, { id, status }) => {
     const index = store.findIndex((data) => data.id === id);
     if(index >=0) {
       return [...store.slice(0, index), { ...store[index], status }, ...store.slice(index + 1)] as Invoice[];
+    }
+    return store;
+  }),
+
+  on(Actions.update, (store, { id, ...data }) => {
+    const index = store.findIndex((data) => data.id === id);
+    if (index >= 0) {
+      return [
+        ...store.slice(0, index),
+        { ...data, id },
+        ...store.slice(index + 1),
+      ]
     }
     return store;
   }),
