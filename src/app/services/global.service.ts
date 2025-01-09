492526838b5ebe9de17a13c5b0c31349a0c2043e
KBismark <bismark.yamoah@amalitech.com>
2025-01-09 16:13:46 +0000

    Add a reset fuction to reset form

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -10,4 +10,20 @@ export class GlobalService {
   adding = false
   editing = false;
   deleting = false;
+
+  idCounts = 0;
+  generateId() {
+    // Generate two random alphabets
+    const randomAlphabets = Math.random().toString(36).substring(2, 4);
+    // Generate four numbers by increasing the idCounts by 1
+    this.idCounts += 1;
+    const randomNumbers = this.idCounts.toString().padStart(4, '0');
+    return `${randomAlphabets}${randomNumbers}`.toUpperCase();
+  }
+
+  resetForm() {
+    this.adding = false;
+    this.editing = false;
+    this.deleting = false;
+  }
 }
